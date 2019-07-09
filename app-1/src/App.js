7487df17861d56d0892431623a9c17184a3807e5
micahhowe <micahhowe182@gmail.com>
2019-07-09 14:05:31 -0600

    app1 is working

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      message: ''
+  }
+}
+    handleChange(value){
+      this.setState({message:value})
+    }
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+         <input type='text' onChange={e => this.handleChange(e.target.value)}/> 
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <p>{this.state.message}</p>
       </div>
     );
   }
